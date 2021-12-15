aa467b4a0220b1b3241ba1633bf82fb01cafc09b
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2021-12-15 02:13:26 -0300

    main Blazored LocalStorage implementation

--- BlazorToDo/Program.cs
+++ BlazorToDo/Program.cs
@@ -2,14 +2,24 @@ using BlazorToDo;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using Blazored.LocalStorage;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-//Add MudBlazor Reference
-builder.Services.AddMudServices();
+//Configure custom services
+ConfigureServices(builder.Services);
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services)
+{
+    //Add MudBlazor Reference
+    services.AddMudServices();
+
+    //Add LocalStorage Service
+    services.AddBlazoredLocalStorage();
+}
