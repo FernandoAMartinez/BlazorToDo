4db725d42c6dba4d1f1b213b570865726d79d441
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2021-12-15 01:50:16 -0300

    Add project files.

---
+++ BlazorToDo/Models/TaskModel.cs
@@ -0,0 +1,14 @@
+﻿namespace BlazorToDo.Models
+{
+    public class TaskModel
+    {
+        public TaskModel() { }
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public string[] Tags { get; set; }
+        public bool Completed { get; set; }
+    }
+}
