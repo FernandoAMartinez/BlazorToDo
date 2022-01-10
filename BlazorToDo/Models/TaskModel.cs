bd2199b1e9b7f05bbbf760ce65bad3ff911ce953
Fernando Martinez <39782229+FernandoAMartinez@users.noreply.github.com>
2022-01-09 22:25:38 -0300

    #9 DataAnnotations for Task Validation

--- BlazorToDo/Models/TaskModel.cs
+++ BlazorToDo/Models/TaskModel.cs
@@ -1,13 +1,15 @@
-﻿namespace BlazorToDo.Models
+﻿using System.COmponentModel.DataAnnotations;
+
+namespace BlazorToDo.Models
 {
     public class TaskModel
     {
         public TaskModel() { }
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        [Required] public string Title { get; set; }
+        [Required] public string Description { get; set; }
         public DateTime CreationDate { get; set; }
-        public DateTime DueDate { get; set; }
+        [Required] public DateTime DueDate { get; set; }
         public Urgency ReportedUrgency { get; set; }
         public string[] Tags { get; set; }
         public bool Completed { get; set; }
